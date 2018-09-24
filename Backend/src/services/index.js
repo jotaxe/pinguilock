e7ae1bfe479a6d6d8409c5fed85ab01a15bc8b96
francisco <franciscoolivaresrivera@gmail.com>
2018-09-24 16:19:58 -0300

    Corregido configuracion servicio usuario

--- Backend/src/services/index.js
+++ Backend/src/services/index.js
@@ -1,4 +1,4 @@
-
+const user = require('./user/user.service.js');
 const face = require('./face/face.service.js');
 const otp = require('./otp/otp.service.js');
 const key = require('./key/key.service.js');
@@ -6,6 +6,7 @@ const accessRequest = require('./access_request/access_request.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(face);
+  app.configure(user);
   app.configure(otp);
   app.configure(key);
   app.configure(accessRequest);
