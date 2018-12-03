96bc56a558c26e80d2c5b8cf273924b8e4cbaebf
Juan Edwards <juan.edwards@mail.udp.cl>
2018-12-03 19:06:16 -0300

    a

--- Backend/src/hooks/retrieve-admin.js
+++ Backend/src/hooks/retrieve-admin.js
@@ -11,16 +11,16 @@ module.exports = function (options = {}) {
         admin: user
       }
     });
-
-    const localServers = adminLocalServers ? adminLocalServers.data.map((obj) => obj.id) : null;
-    const adminLock = localServer ? await context.app.service('lock').find(
+    console.log(adminLocalServers);
+    const localServers = adminLocalServers.data.map((obj) => obj.id);
+    const adminLock = await context.app.service('lock').find(
       {
         $select: ["topic", "local_server_id", "id"],
         local_server_id: {
           $in: localServers
         }
       }
-    ): null;
+    );
 
     const userKeys = await context.app.service('key').find(
       {
