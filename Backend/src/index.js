473aa4fc03a5d5a77ef9051535aea6dc05d7bea1
Juan Edwards <juan.edwards@mail.udp.cl>
2018-12-04 20:08:43 -0300

    https y http

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,19 +3,25 @@ const logger = require('./logger');
 const app = require('./app');
 const port = app.get('port');
 
-const server = https.createServer({
+const httpsServer = https.createServer({
   key: fs.readFileSync('/etc/letsencrypt/live/pinguilock.tk/privkey.pem'),
   cert: fs.readFileSync('/etc/letsencrypt/live/pinguilock.tk/cert.pem')
 }, app).listen(443);
 
-app.setup(server);
+app.setup(httpsServer);
 
+const httpServer = app.listen(port);
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
 );
 
-server.on('listening', () =>{
+httpsServer.on('listening', () =>{
+  //logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
+  console.log(`Pinguilock backend server started on https://${app.get('host')}`);
+});
+
+httpServer.on('listening', () =>{
   //logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
   console.log(`Pinguilock backend server started on http://${app.get('host')}:${port}`);
 });
