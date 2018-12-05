a77ed8f481fe71765ac9cd691d06da2f0737d282
Juan Edwards <juan.edwards@mail.udp.cl>
2018-12-04 21:15:18 -0300

    arreglos finales... ahora si

--- Hardware/RaspberryPi/localserver/src/externalApi/extApp.js
+++ Hardware/RaspberryPi/localserver/src/externalApi/extApp.js
@@ -9,7 +9,7 @@ let app = feathers().configure(conf);
 const socketio = require('@feathersjs/socketio-client');
 const io = require('socket.io-client');
 const extApp = feathers();
-const socket = io(app.get('externalApi').url, {secure: true}); //direccion de la api
+const socket = io(app.get('externalApi').url); //direccion de la api
 const auth = require('@feathersjs/authentication-client');
 const {username, password} = app.get('externalApi').credentials;
 //app.configure(restClient.fetch(window.fetch));
